f48bf2dade9ca00543ec272626a81b20b9c53c41
IlicStefan <stefanilic288@gmail.com>
2020-07-09 23:01:15 +0200

    Add buttons

--- src/Application.py
+++ src/Application.py
@@ -1,46 +1,61 @@
+################################################################################
+# Main application
+################################################################################
+from os.path import abspath, dirname, join
+import sys
+sys.path.insert(0, join(abspath(dirname(__file__)), "../tools"))
+
+from relative_to_absolute_path import get_absolute_path
+import tkinter.filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
+
+################################################################################
+################################################################################
 
 
 def load_pieces() -> dict:
     return {
         "bB": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bB.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bB.png", __file__)).resize((70, 70))
         ),
         "bK": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bK.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bK.png", __file__)).resize((70, 70))
         ),
         "bN": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bN.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bN.png", __file__)).resize((70, 70))
         ),
         "bP": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bP.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bP.png", __file__)).resize((70, 70))
         ),
         "bQ": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bQ.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bQ.png", __file__)).resize((70, 70))
         ),
         "bR": ImageTk.PhotoImage(
-            Image.open("resources/pieces/bR.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/bR.png", __file__)).resize((70, 70))
         ),
         "wB": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wB.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wB.png", __file__)).resize((70, 70))
         ),
         "wK": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wK.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wK.png", __file__)).resize((70, 70))
         ),
         "wN": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wN.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wN.png", __file__)).resize((70, 70))
         ),
         "wP": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wP.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wP.png", __file__)).resize((70, 70))
         ),
         "wQ": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wQ.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wQ.png", __file__)).resize((70, 70))
         ),
         "wR": ImageTk.PhotoImage(
-            Image.open("resources/pieces/wR.png").resize((70, 70))
+            Image.open(get_absolute_path("../resources/pieces/wR.png", __file__)).resize((70, 70))
         ),
     }
+
+################################################################################
+################################################################################
 
 
 class ChessBoard(tk.Frame):
@@ -50,13 +65,37 @@ class ChessBoard(tk.Frame):
         self.canvas = tk.Canvas(self, width=560, height=560)
         self.canvas.place(x=20, y=20)
 
-        self.board_image = ImageTk.PhotoImage(Image.open("resources/board.jpg").resize((560, 560)))
+        self.board_image = ImageTk.PhotoImage(
+            Image.open(get_absolute_path("../resources/board.jpg", __file__)).resize((560, 560))
+        )
         self.canvas.create_image(0, 0, image=self.board_image, anchor=tk.NW)
 
         self.pieces = load_pieces()
         self.rook = self.pieces["wR"]
         self.canvas.create_image(0, 0, image=self.rook, anchor=tk.NW)
 
+
+################################################################################
+################################################################################
+
+
+class LeftSide(tk.Frame):
+    def __init__(self, root, *args, **kwargs):
+        tk.Frame.__init__(self, root, *args, **kwargs)
+
+        self.camera_button = tk.Button(self, text="Open camera", command= lambda: print("Camera"))
+        self.camera_button.place(x=50, y=20)
+
+        self.browse_button = tk.Button(self, text="Browse file", command=lambda: print("Browse"))
+        self.browse_button.place(x=200, y=20)
+
+        self.canvas = tk.Canvas(self, width=400, height=400, background="Red")
+        self.canvas.place(x=50, y=150)
+
+        # self.board_image = ImageTk.PhotoImage(
+        #     Image.open(get_absolute_path("../resources/board.jpg", __file__)).resize((400, 400))
+        # )
+        # self.canvas.create_image(0, 0, image=self.board_image, anchor=tk.NW)
 
 
 ################################################################################
@@ -70,7 +109,7 @@ class Application(tk.Frame):
         self.root = root
         self.root.title("Chess Diagram Recognition")
         
-        self.left_side = tk.Frame(self.root, height=600, width=600, background="Red")
+        self.left_side = LeftSide(self.root, height=600, width=600)
         self.left_side.place(x=0, y=0)
 
         self.chess_board = ChessBoard(self.root, height=600, width=600)
